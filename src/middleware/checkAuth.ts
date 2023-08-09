7710f5e7ea0a90e8316af08a399d98397ef2037b
GregDevLab <g.batte@outlook.fr>
2023-08-09 02:21:55 +0200

    Mise en place d'un suivie de l'id utilisateur depuis le middleware CheckAuth

--- src/middleware/checkAuth.ts
+++ src/middleware/checkAuth.ts
@@ -1,10 +1,19 @@
 import { TokenHandler } from "@src/utils";
 import { NextFunction, Request, Response } from "express";
 
+interface IUser {
+	[key: string]: any;
+}
+declare module 'express-serve-static-core' {
+	interface Request {
+		user?: IUser;
+	}
+}
 const checkAuth = async (req: Request, res: Response, next: NextFunction) => {
 	try {
 		const token = req.cookies.token
-		await TokenHandler.verifyToken(token)
+		const user = await TokenHandler.verifyToken(token)
+		req.user = user
 		next()
 	}catch (err) {
 		res.status(403).json({ message: 'Token invalide' })
